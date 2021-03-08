e5f629291e0beaa9401485c2abcb7bf39850fe3c
Derek Wang <derekwangsz@gmail.com>
2021-03-08 09:39:06 -0800

    Added one player

--- Source Control test/GameScene.swift
+++ Source Control test/GameScene.swift
@@ -11,5 +11,13 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        let player1 = SKSpriteNode(imageNamed: "player1")
+        player1.setScale(0.5)
+        player1.position = CGPoint(x: 0, y: 0)
+        player1.zPosition = 1
+        addChild(player1)
     }
 }
